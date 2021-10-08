4d57659ddad277d8985274e88b91cea5d71e423d
ksathwik03 <iit2020241@iiita.ac.in>
2021-10-09 02:24:47 +0530

    Added env

--- con-main/Data_base.py
+++ con-main/Data_base.py
@@ -1,20 +1,22 @@
+import os
+
 import pymongo
 
 
 db = []
 
-
+Password = os.getenv('Password')
 def data_base():
     global db
     db = pymongo.MongoClient(
-        "mongodb+srv://Ksathwik03:Ksathwik03@cluster0.xtzux.mongodb.net/Pythin?retryWrites=true&w=majority")
+        f'mongodb+srv://Ksathwik03:{Password}@cluster0.xtzux.mongodb.net/Pythin?retryWrites=true&w=majority')
     db = db['testing']
     db = db['channel']
     return db
 
 def error_data_base():
     error = pymongo.MongoClient(
-        "mongodb+srv://Ksathwik03:Ksathwik03@cluster0.xtzux.mongodb.net/Pythin?retryWrites=true&w=majority")
+        f"mongodb+srv://Ksathwik03:{Password}@cluster0.xtzux.mongodb.net/Pythin?retryWrites=true&w=majority")
     error = error['testing']
     error = error['error']
     return error
